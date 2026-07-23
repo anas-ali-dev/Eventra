93ac06fa1365835e530ad4f913faf97985f71161
unknown <your.aliaamahmoudmohamed7@gmail.com>
2026-07-23 17:20:19 +0300

    add tokens

---
+++ Backend/Utils/generateAccessToken.js
@@ -0,0 +1,12 @@
+import jwt from "jsonwebtoken";
+
+const generateAccessToken = (userId, role) =>
+{
+    return jwt.sign(
+        { id: userId, role },
+        process.env.JWT_ACCESS_SECRET,
+        { expiresIn: process.env.JWT_ACCESS_EXPIRE || "15m" }
+    );
+};
+
+export default generateAccessToken;
